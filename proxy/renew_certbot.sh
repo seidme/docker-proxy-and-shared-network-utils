0602d4118d84b2cbc058b2705d8b8efe4a345291
seidme <mehmedovic.seid@gmail.com>
2026-03-04 17:45:47 +0100

    fix: set renew_certbot.sh as executable

--- proxy/renew_certbot.sh
+++ proxy/renew_certbot.sh
@@ -5,15 +5,15 @@
 CERTBOT_ETC="./certbot/etc"
 
 echo "=== Stopping nginx-proxy ==="
-docker stop nginx-proxy
+/usr/bin/docker stop nginx-proxy
 
 echo "=== Running certbot renew on port 80 (standalone mode) ==="
-docker run --rm \
+/usr/bin/docker run --rm \
   -p 80:80 \
   -v "$(pwd)/certbot/etc:/etc/letsencrypt" \
   certbot/certbot renew
 
 echo "=== Starting nginx-proxy ==="
-docker start nginx-proxy
+/usr/bin/docker start nginx-proxy
 
 echo "=== Renewal complete ==="
